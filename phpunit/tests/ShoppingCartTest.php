d231024807df6296d3b42f086a8ea572dbd2bdc2
JGomes <zx.gomes@gmail.com>
2017-08-23 11:30:30 +0100

    Update ShoppingCartTest.php

--- phpunit/tests/ShoppingCartTest.php
+++ phpunit/tests/ShoppingCartTest.php
@@ -4,7 +4,7 @@ class ShoppingCartTest extends PHPUnit_Framework_TestCase {
 
     public function testAddItemStub()
     {   // dummy test
-        $this->assertTrue(true);
+        $this->assertTrue(false);
     }
 
     public function testTotalAddedItemsStub()
