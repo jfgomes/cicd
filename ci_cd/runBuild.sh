c51a500a66e0c4e48f390faeaa27a9761e5fac98
= <jose.gomes@gmail.com>
2018-02-20 13:31:26 +0000

    test

--- ci_cd/runBuild.sh
+++ ci_cd/runBuild.sh
@@ -8,4 +8,10 @@ cd /var/www && php composer-setup.php
 cd /var/www && php -r "unlink('composer-setup.php');"
 cd /var/www && chmod 777 composer.phar
 cd /var/www && php composer.phar update
+
+wget https://phar.phpunit.de/phpunit-6.5.phar
+chmod +x phpunit-6.5.phar
+sudo mv phpunit-6.5.phar /usr/local/bin/phpunit
+phpunit --version
+
 exit 0
