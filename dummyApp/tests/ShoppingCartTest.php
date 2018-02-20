d1ffd9b2eb0a87866316357677cb1a218484e3f8
= <jose.gomes@gmail.com>
2018-02-20 14:12:01 +0000

    testing

--- dummyApp/tests/ShoppingCartTest.php
+++ dummyApp/tests/ShoppingCartTest.php
@@ -2,11 +2,19 @@
 
 class ShoppingCartTest extends \PHPUnit\Framework\TestCase {
 
+/**
+ * @test
+ * @covers Shopping::testAddItemStub
+ */
     public function testAddItemStub()
     {   // dummy test
         $this->assertTrue(true);
     }
 
+/**
+ * @test
+ * @covers Shopping::testTotalAddedItemsStub
+ */
     public function testTotalAddedItemsStub()
     {   // dummy test
         $this->assertTrue(true);
